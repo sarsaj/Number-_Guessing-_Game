45d0efe4a9500098cd5cbd096ee883ce405597bb
Sanjeeda Kanwal <160398514+sarsaj@users.noreply.github.com>
2024-03-23 09:00:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const random_Number = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "user_Guess_number",
+        type: "number",
+        message: "Please guess a number between 1-6:",
+    },
+]);
+if (answers.user_Guess_number === random_Number) {
+    console.log("Congratulation you have guess a right number.");
+}
+else {
+    console.log("You loose! Guess a wrong number. ");
+}
